956b829ef8757b7ab0bb9c494164f784c523d787
jina jeong <jina5389@gmail.com>
2026-05-27 03:06:32 +0900

    meta description 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const viewport: Viewport = {
 
 export const metadata: Metadata = {
   title: '정지나 | Web Publisher',
-  description: '웹 퍼블리셔 포트폴리오입니다.',
+  description: '웹 퍼블리셔 정지나입니다.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
