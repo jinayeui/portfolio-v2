62aad01cf732aceed0db099ebc31e5d589ee39c4
jina jeong <jina5389@gmail.com>
2026-06-16 00:05:26 +0900

    렌더링 성능 개선: setTimeout+rAF

--- hooks/useActiveOnMount.ts
+++ hooks/useActiveOnMount.ts
@@ -4,9 +4,11 @@ import { useEffect } from 'react';
 
 export function useActiveOnMount() {
   useEffect(() => {
-	const elements = document.querySelectorAll('.target-mount');
-	setTimeout(() => {
-		elements.forEach((el) => el.classList.add('is-active'));
-	}, 300);
+    const elements = document.querySelectorAll('.target-mount');
+    setTimeout(() => {
+      requestAnimationFrame(() => {
+        elements.forEach((el) => el.classList.add('is-active'));
+      });
+    }, 300);
   }, []);
 }
